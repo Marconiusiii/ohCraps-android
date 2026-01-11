010e1ebf4b0b174dce548616d69d9ee505ce9ecb
Marco Salsiccia <marco.salsiccia@gmail.com>
2026-01-11 14:07:02 -0800

    Starting to figure out the Android Studio interface. Cleared Hello Android text from main Activity

--- app/src/main/java/com/marconius/ohcraps/MainActivity.kt
+++ app/src/main/java/com/marconius/ohcraps/MainActivity.kt
@@ -3,45 +3,23 @@ package com.marconius.ohcraps
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.marconius.ohcraps.ui.theme.OhCrapsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
-            OhCrapsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            OhCrapsApp()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    OhCrapsTheme {
-        Greeting("Android")
+fun OhCrapsApp() {
+    MaterialTheme {
+        Text(text = "Oh Craps Android App")
     }
 }
