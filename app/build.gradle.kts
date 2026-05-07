9fb9b7b1ad564c74931fb57c32e09705e3bf4d49
Marco Salsiccia <marco.salsiccia@gmail.com>
2026-05-06 20:15:17 -0700

    Removed chips from chip rail to hopefully balance the bottom nav labels

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
 		applicationId = "com.marconius.ohcraps"
 		minSdk = 23
 		targetSdk = 36
-		versionCode = 7
-		versionName = "1.2.1"
+		versionCode = 8
+		versionName = "1.2.2"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 	}
